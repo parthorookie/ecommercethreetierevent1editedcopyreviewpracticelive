4504ce4d9132e8fcd4b2d7256043bdfac2c33d40
Partho M <parthomazumdar999@gmail.com>
2026-04-16 04:15:17 +0530

    success test done sure 37

---
+++ terraform/modules/ecr/outputs.tf
@@ -0,0 +1,11 @@
+output "backend_repo_url" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "worker_repo_url" {
+  value = aws_ecr_repository.worker.repository_url
+}
+
+output "frontend_repo_url" {
+  value = aws_ecr_repository.frontend.repository_url
+}
