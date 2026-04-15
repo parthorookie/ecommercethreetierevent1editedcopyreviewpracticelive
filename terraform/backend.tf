946d70b58a3fc7917c134ccbe4934e8741faedca
Partho M <parthomazumdar999@gmail.com>
2026-04-16 01:30:39 +0530

    success test done sure 22

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.10.0"
 
   backend "s3" {
-    bucket       = "parthoecom--aps1-az1--x-s3"
+    bucket       = "partho-253490759114-ap-south-1-an"
     key          = "terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
