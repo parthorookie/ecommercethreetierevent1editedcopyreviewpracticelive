6eed826a00cf961c409ccf293887478b423961fd
Partho M <parthomazumdar999@gmail.com>
2026-04-16 02:30:00 +0530

    success test done sure 26

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.10.0"
 
   backend "s3" {
-    bucket       = "ecommerce-terraform-state-prod2"
+    bucket       = "parth--aps1-az1--x-s3"
     key          = "terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
