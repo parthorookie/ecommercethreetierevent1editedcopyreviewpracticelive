9108aa57aef63fbf3f8f94a880d0a99c1d65dd99
Partho M <parthomazumdar999@gmail.com>
2026-04-16 00:49:48 +0530

    success test done sure 18

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket       = "ecommerce-terraform-state-prod-partho-001"
     key          = "terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "ap-southeast-1"
     encrypt      = true
     use_lockfile = true
   }
